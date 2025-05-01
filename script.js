4f45b6a53f4dacb195a46794c0d2bad895d23ac1
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-05-01 19:53:49 +0200

    ...

--- script.js
+++ script.js
@@ -1,1 +1,95 @@
 "use strict";
+
+const friendsList = document.querySelector(".friends-list");
+const searchInput = document.querySelector(".find-friends");
+const suggestionsBox = document.querySelector(".search-suggestions");
+
+const friends = [
+  { name: "Emily Johnson", picture: "profile1.jpg" },
+  { name: "Michael Smith", picture: "profile2.jpg" },
+  { name: "Sophia Williams", picture: "profile3.jpg" },
+  { name: "James Brown", picture: "profile4.jpg" },
+  { name: "Olivia Jones", picture: "profile5.jpg" },
+  { name: "Daniel Miller", picture: "profile6.jpg" },
+];
+
+class Friend {
+  constructor(name, picture) {
+    this.name = name;
+    this.picture = picture;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getPicture() {
+    return this.picture;
+  }
+}
+
+class FriendsManager {
+  constructor() {
+    this.myFriends = [];
+  }
+
+  addFriend(friend) {
+    this.myFriends.push(friend);
+  }
+
+  renderFriend(friend) {
+    const friendCard = document.createElement("div");
+    friendCard.classList.add("friend-card");
+
+    friendCard.innerHTML = `
+      <img src="${friend.getPicture()}" alt="${friend.getName()}" class="friend-img" />
+      <p class="friend-name">${friend.getName()}</p>
+    `;
+
+    friendsList.appendChild(friendCard);
+  }
+
+  renderAllFriends() {
+    friendsList.innerHTML = "";
+    this.myFriends.forEach((friend) => this.renderFriend(friend));
+  }
+
+  searchFriends(query) {
+    return this.myFriends.filter((friend) =>
+      friend.getName().toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+}
+
+const manager = new FriendsManager();
+
+friends.forEach((data) => {
+  const newFriend = new Friend(data.name, data.picture);
+  manager.addFriend(newFriend);
+});
+
+manager.renderAllFriends();
+
+searchInput.addEventListener("input", (e) => {
+  const query = e.target.value.trim();
+  const matches = manager.searchFriends(query);
+
+  suggestionsBox.innerHTML = "";
+
+  if (query !== "" && matches.length > 0) {
+    matches.forEach((friend) => {
+      const item = document.createElement("div");
+      item.classList.add("suggestion-item");
+
+      item.innerHTML = `
+        <img src="${friend.getPicture()}" class="suggestion-img" />
+        <span class="suggestion-name">${friend.getName()}</span>
+      `;
+
+      suggestionsBox.appendChild(item);
+    });
+    suggestionsBox.hidden = false;
+  } else {
+    suggestionsBox.hidden = true;
+  }
+});
