6b44a1662c4fb0d981d9ec4a201d2d4ccd34e211
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-05-28 22:13:30 +0200

    ...

--- script.js
+++ script.js
@@ -43,11 +43,16 @@ function setupRenderedPost(post, li) {
       <div class="comment-user">
         <img class="post-img" src="${comment.user.picture}" />
          <div class="comment-style">
-         <button class="dot-com-btn">...</button>
           <span class="comment-name">${comment.user.name}</span>
           <span class="comment-text">${comment.text}</span>
         <div class="like-delete-comment">
+            <div class="like-com-div">
             <button class="like-comment">I like it.</button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+</svg>
+<span class="likes-number">${comment.commentLikes.length}</span>
+          </div>
             <button class="delete-comment">Delete comment.</button>
           </div>
         </div>
@@ -125,7 +130,21 @@ fixedPosts.forEach((data) => {
 
   data.comments.forEach((c) => {
     const commenter = new User(c.profile, c.image, "");
-    post.comments.push(new Comment(commenter, c.text));
+    const comment = new Comment(commenter, c.text);
+
+    const fakeLiker1 = new User("Test User 1", "test1.png");
+    const fakeLiker2 = new User("Test User 2", "test2.png");
+
+    comment.commentLikes.push(new Like(fakeLiker1), new Like(fakeLiker2));
+
+    if (c.likes) {
+      c.likes.forEach((l) => {
+        const liker = new User(l.name, l.image, "");
+        comment.commentLikes.push(new Like(liker));
+      });
+    }
+
+    post.comments.push(comment);
   });
 
   currentUser.posts.push(post);
@@ -217,11 +236,20 @@ postList.addEventListener("click", function (e) {
       <div class="comment-user">
         <img class="post-img" src="${currentUser.picture}" />
         <div class="comment-style">
-        <button class="dot-com-btn">...</button>
+        <button class="edit-com-btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+</svg>
+</button>
           <span class="comment-name">${currentUser.name}</span>
           <span class="comment-text">${text}</span>
           <div class="like-delete-comment">
+          <div class="like-com-div">
             <button class="like-comment">I like it.</button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+</svg>
+<span class="likes-number">${comment.commentLikes.length}</span>
+          </div>
             <button class="delete-comment">Delete comment.</button>
           </div>
         </div>
@@ -285,12 +313,29 @@ postList.addEventListener("click", function (e) {
   if (e.target.classList.contains("like-comment")) {
     const commentLi = e.target.closest(".single-comment");
     if (!commentLi) return;
+
     const commentIndex = Number(commentLi.dataset.commentId);
     const comment = post.comments[commentIndex];
     if (!comment) return;
 
+    const likeBtn = commentLi.querySelector(".like-comment");
+    const likesNumber = commentLi.querySelector(".likes-number");
+
     comment.toggleLike(currentUser);
-    e.target.classList.toggle("liked-comment", comment.isLikedBy(currentUser));
+
+    if (comment.isLikedBy(currentUser)) {
+      likeBtn.classList.add("liked-comment");
+      likeBtn.style.color = "rgb(0, 136, 255);";
+    } else {
+      likeBtn.classList.remove("liked-comment");
+      likeBtn.style.color = "";
+    }
+
+    const svgIcon = commentLi.querySelector(".like-com-div svg");
+    if (svgIcon)
+      svgIcon.style.stroke = comment.isLikedBy(currentUser) ? "blue" : "";
+
+    likesNumber.textContent = comment.commentLikes.length;
   }
 
   if (e.target.classList.contains("delete-comment")) {
@@ -311,5 +356,44 @@ postList.addEventListener("click", function (e) {
     commentCount.textContent = `${post.comments.length} comment${
       post.comments.length === 1 ? "" : "s"
     }`;
+  }
+
+  if (e.target.closest(".edit-com-btn")) {
+    const editBtn = e.target.closest(".edit-com-btn");
+    const commentLi = editBtn.closest(".single-comment");
+    if (!commentLi) return;
+
+    const commentIndex = Number(commentLi.dataset.commentId);
+    const comment = post.comments[commentIndex];
+    if (!comment) return;
+
+    if (comment.user !== currentUser) return;
+
+    const existingInput = commentLi.querySelector(".edit-comment-input");
+
+    if (existingInput) {
+      const newText = existingInput.value.trim();
+      if (newText !== "") {
+        comment.text = newText;
+      }
+
+      const newSpan = document.createElement("span");
+      newSpan.className = "comment-text";
+      newSpan.textContent = comment.text;
+
+      existingInput.replaceWith(newSpan);
+      editBtn.classList.remove("editing");
+    } else {
+      const commentTextSpan = commentLi.querySelector(".comment-text");
+      if (!commentTextSpan) return;
+
+      const input = document.createElement("input");
+      input.type = "text";
+      input.value = comment.text;
+      input.className = "edit-comment-input";
+
+      commentTextSpan.replaceWith(input);
+      editBtn.classList.add("editing");
+    }
   }
 });
